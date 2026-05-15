79b43522a7a2efaf80eea92ed20bd33bb4af1f55
PrathameshNJ <prathameshjagtap607@gmail.com>
2026-05-15 15:35:23 +0530

    [TASK-011] feat: create NotificationsModule and wire dependencies

--- app/notifications/notifications.module.ts
+++ app/notifications/notifications.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
+import Redis from 'ioredis';
 import { PrismaModule } from '../prisma/prisma.module';
+import { MailService } from '../mail/mail.service';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
+import { RateLimiterService } from './rate-limiter.service';
+import { NotificationHandler } from './notification.handler';
+import { DigestCronJob } from './digest-cron.job';
 
 @Module({
   imports: [PrismaModule],
   controllers: [NotificationsController],
-  providers: [NotificationsService],
-  exports: [NotificationsService],
+  providers: [
+    NotificationsService,
+    RateLimiterService,
+    NotificationHandler,
+    DigestCronJob,
+    MailService,
+    {
+      provide: 'REDIS_CLIENT',
+      useFactory: () => new Redis(process.env['REDIS_URL'] ?? 'redis://localhost:6379'),
+    },
+  ],
+  exports: [NotificationsService, NotificationHandler],
 })
 export class NotificationsModule {}
